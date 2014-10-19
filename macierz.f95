5f461604a85755393693082fe51a73bbe9e46889
Jakub Sylburski <jakubsylburski@gmail.com>
2014-10-19 23:06:23 +0200

    add neww code

--- macierz.f95
+++ macierz.f95
@@ -1,27 +1,41 @@
 PROGRAM Mnozenie
 	integer, parameter :: X = 10;
 	integer, parameter :: Y = 10;
-	integer :: menu;
+	integer :: menu, n;
 	
 	do
-	
-	write (*,*) '================================================'
-    write (*,*) ' Menu'
-    write (*,*) '================================================'
-    write (*,*) ' 1. Mnozenie macierzy 10x10'
-    write (*,*) ' 2. wyjscie z programu'
-    write (*,*) ' Wybierz opcje:'
-    read(*,*) menu
-	
-	select case (menu)
-		case (1)
-			print *, "Do zrobienia"
-		case (2)
-			exit
-		case default
-			print *, "Liczba z poza zakresu"
-	end select
-	
+		write (*,*) '================================================'
+    	write (*,*) ' Menu'
+    	write (*,*) '================================================'
+    	write (*,*) ' 1. Mnozenie macierzy 10x10'
+    	write (*,*) ' 2. wyjscie z programu'
+    	write (*,*) ' Wybierz opcje:'
+    	read(*,*) menu
+		
+		select case (menu)
+			case (1)
+				print *, "Do zrobienia"
+               !n=multiplication( );
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+    i=0;
+    do
+		j=0;
+    	do
+            m1(i,j) = 1;
+            m2(i,j) = 1;
+            j=j+j;
+            if(j == Y) exit
+        end do
+        
+        i=i+1;
+        if(i == X) exit
+    end do
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			case (2)
+				exit
+			case default
+				print *, "Liczba z poza zakresu"
+		end select
 	end do
 
 END
@@ -34,8 +48,17 @@ real function print_for_multiplication( tab )
 end function
 
 real function multiplication( )
+
+	integer :: m1(10,10), m2(10,10), w(10,10), i=0, j=0, k=0, tmp=0;
 	
+!	call init_random_seed() ! see example of random_seed
+!	call random_number(w)
 	
+!	print *, w(3,5)
+    
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	
 	return
 end function
