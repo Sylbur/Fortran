f7581035e06c0988c388e7af6486748cef67be65
Jakub Sylburski <jakubsylburski@gmail.com>
2014-10-25 06:16:25 +0200

    update code macierz.f95

--- macierz.f95
+++ macierz.f95
@@ -1,70 +1,25 @@
 PROGRAM Mnozenie
 	integer, parameter :: X = 10;
 	integer, parameter :: Y = 10;
-	integer :: menu, n;
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	integer :: m1(10,10), m2(10,10),w(10,10), i=0, j=0, k=0, tmp=0;
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	
+	integer :: menu;
+    real :: n;
+
 	do
 		write (*,*) '================================================'
     	write (*,*) ' Menu'
     	write (*,*) '================================================'
     	write (*,*) ' 1. Mnozenie macierzy 10x10'
     	write (*,*) ' 2. wyjscie z programu'
+        write (*,*) ' 0. wyjscie z programu'
     	write (*,*) ' Wybierz opcje:'
     	read(*,*) menu
 		
 		select case (menu)
 			case (1)
-				print *, "Do zrobienia"
-               !n=multiplication( );
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
-	
-	
-!	call init_random_seed() ! see example of random_seed
-!	call random_number(w)
-	
-!	print *, w(3,5)
-    
-    i=0;
-    do
-		j=0;
-    	do
-!			m1(i,j) = 1;
-!			m2(i,j) = 1;
-            j=j+1;
-            if(j == Y) exit
-        end do
-        
-        i=i+1;
-        if(i == X) exit
-    end do
-
-    i=0;
-    do
-		j=0;
-    	do
-        	k=0;
-			do
-!				tmp = tmp + m1(i,k) * m2(k,j);
-                k=k+1;
-               	if(k == X) then !POTEM POPRAWI
-!					w(i,j) = tmp;
-                    tmp = 0;
-               		exit
-                end if
-           	end do
-            
-			j=j+1;
-        	if(j == Y) exit
-        end do
-        
-        i=i+1;
-        if(i == X) exit
-    end do
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+               	n=func_multiplication();
 			case (2)
+                n=func_metoda_lu();
+			case (0)
 				exit
 			case default
 				print *, "Liczba z poza zakresu"
@@ -73,18 +28,86 @@ PROGRAM Mnozenie
 
 END
 
-real function print_for_multiplication( tab )
-	
-	
-	
-	return
-end function
+real function func_multiplication ( ) 
+	implicit none
+    
+	integer, parameter :: X = 10, Y = 10;
+	integer :: m1(10,10), m2(10,10), w(10,10), i=0, j=0, k=0, tmp=0;
+	real :: n;
+!!	call init_random_seed() ! see example of random_seed
+!!	call random_number(w)
 
-real function multiplication( )
+	i=1;
+	do
+		j=1;
+		do
+			m1(i,j) = 1;
+			m2(i,j) = 1;
+			j=j+1;
+			if(j == Y) exit
+		end do
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		i=i+1;
+		if(i == X) exit
+	end do
+    
+!    print *, m1(2,2)
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	i=1;
+	do
+		j=1;
+		do
+			k=1;
+			do
+				tmp = tmp + m1(i,k) * m2(k,j);
+				k=k+1;
+				if(k == X) then !POTEM POPRAWIC
+					w(i,j) = tmp;
+					tmp = 0;
+					exit
+				end if
+			end do
+
+			j=j+1;
+			if(j == Y) exit
+		end do
+
+		i=i+1;
+		if(i == X) exit
+	end do
+
+!	n=func_multiplication_print ( m1 );
+    print *, "Do dokonczenia"
+
+end function func_multiplication
+
+real function func_multiplication_print ( tab )
+	implicit none
 	
-	return
-end function
+	integer, parameter :: X = 10, Y = 10;
+	integer :: tab(10,10), i=0, j=0;
+
+	i=1;
+	do
+		print *, "| "
+		j=1;
+		do
+			print *, tab(i,j)
+            print *, " "
+			j=j+1;
+			if(j == Y) exit
+		end do
+		
+		print *, "|"
+		i=i+1;
+		if(i == X) exit
+	end do
+
+end function func_multiplication_print
+
+real function func_metoda_lu ( )
+	implicit none
+	
+	print *, "Opcja do zrobienia"
+
+end function func_metoda_lu
